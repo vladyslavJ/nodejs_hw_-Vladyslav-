e08f9134dbe4a5ffeca92735d48f69c9aebfcf8a
vladyslavJ <vladzhukov321@gmail.com>
2024-12-16 20:31:44 +0200

    .env, import ES6 and colors.js

---
+++ homework02/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import getLogger from './utils/logger.js';
+
+const { log, warn } = getLogger('main');
+log('the script is running!');
+warn('the script is running!');
---
+++ homework02/utils/logger.js
@@ -0,0 +1,50 @@
+import colors from 'colors/safe.js';
+import config from 'config';
+import 'dotenv/config';
+
+colors.setTheme({
+	info: 'cyan',
+	log: 'green',
+	warn: 'yellow',
+	error: 'red',
+});
+
+let status = parseInt(process.env.COLORS_ENABLED);
+const defaultStatus = config.get('COLORS_ENABLED');
+
+if (Number.isNaN(status) || !(status >= 0 && status <= 1)) {
+	console.warn(
+		colors.warn(
+			'process.env.COLORS_ENABLED does not meet requirements. Using default value.'
+		)
+	);
+	status = defaultStatus;
+}
+
+const getLogger = (namespace) => {
+	(getLogger.info = (message) => {
+		if (status === 0) {
+			console.log(`${namespace}: ${message}`);
+		} else {
+			console.log(colors.info(`${namespace}: `) + message);
+		}
+	}),
+		(getLogger.log = (message) => {
+			if (status === 0) {
+				console.log(`${namespace}: ${message}`);
+			} else {
+				console.log(colors.log(`${namespace}: `) + message);
+			}
+		}),
+		(getLogger.warn = (message) => {
+			if (status === 0) {
+				console.error(`${namespace}: ${message}`);
+			} else {
+				console.error(colors.warn(`${namespace}: `) + message);
+			}
+		});
+
+	return getLogger;
+};
+
+export default getLogger;
